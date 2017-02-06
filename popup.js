b2e7515211c7dd7376632d030219fd08db7ced47
Nikita Chernykh <chernykhmain@gmail.com>
2017-02-05 22:17:05 -0500

    Added styles and no result place older

--- popup.js
+++ popup.js
@@ -6,16 +6,24 @@ background.websiteList.sort(function(a, b){
     return b.websiteVisits - a.websiteVisits;
 });
 
+//Place holder for no results
+if(background.websiteList.length == 0){
+  document.write("<div>No addiction image holder</div>");
+}
+      
 //Display list of visited sites
 for (var i = 0; i < background.websiteList.length; i++) {
   //Variables
-  var websiteName = background.websiteList[i].websiteName;
-  var visits = background.websiteList[i].websiteVisits; 
-  var icon = background.websiteList[i].favIcon;
-  document.write(
-    "<tr><td>"
-  + "<img src=" +icon+ " height='20' width='20'></td>"
-  + "<td><a href='http://" +websiteName+ "' target='_blank'> " +websiteName+ "</a>"
-  + " visits: " +visits+"</td><br><hr>"
-  );
+  if(i <5){
+    var websiteName = background.websiteList[i].websiteName;
+    var visits = background.websiteList[i].websiteVisits; 
+    var icon = background.websiteList[i].favIcon;
+    document.write(
+      "<div class='listContainer'><tr><td>"
+    + "<img src=" +icon+ " height='20' width='20'></td>"
+    + "<td><a href='http://" +websiteName+ "' target='_blank'> " +websiteName+ "</a>"
+    + " visits: " +visits+"</td><br></div>"
+    );
+  }
+  
 }
