317eaa98a3000d26da5541ebc06430e0ffec1c65
Nikita Chernykh <chernykhmain@gmail.com>
2017-02-09 00:43:32 -0500

    Added updated css style

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 //Get acsses to the background.js
 var background = chrome.extension.getBackgroundPage();
+var content = document.getElementById("websiteList");
 
 //Sort websites in descending order by visits
 background.websiteList.sort(function(a, b){
@@ -10,20 +11,21 @@ background.websiteList.sort(function(a, b){
 if(background.websiteList.length == 0){
   document.write("<div>No addiction image holder</div>");
 }
-      
+
 //Display list of visited sites
 for (var i = 0; i < background.websiteList.length; i++) {
-  //Variables
+
   if(i <5){
+    //Variables
     var websiteName = background.websiteList[i].websiteName;
     var visits = background.websiteList[i].websiteVisits; 
     var icon = background.websiteList[i].favIcon;
-    document.write(
-      "<div class='listContainer'><tr><td>"
-    + "<img src=" +icon+ " height='20' width='20'></td>"
-    + "<td><a href='http://" +websiteName+ "' target='_blank'> " +websiteName+ "</a>"
-    + " visits: " +visits+"</td><br></div>"
-    );
+    var website = "<a href='http://" +websiteName+ "' target='_blank'><div class='listContainer'>"
+    + "<div class='image'><img src=" +icon+ " height='20' width='20'></div>"
+    + "<div class='item'><span> " +websiteName+ "</span>"
+    + " visits: " +visits+"</div></div></a>";
+    content.innerHTML += website;
   }
+  
   
 }
