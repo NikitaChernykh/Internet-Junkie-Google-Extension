2e750ccf1ac52b9e868b3a97f4cfea60f7abc9ed
Nikita Chernykh <chernykhmain@gmail.com>
2017-02-13 17:50:03 +0000

    added analitycs

--- popup.js
+++ popup.js
@@ -1,3 +1,16 @@
+//Google Analytics Start
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-91876786-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+//Google Analytics End
+
+
 //Get acsses to the background.js
 var background = chrome.extension.getBackgroundPage();
 var content = document.getElementById("websiteList");
