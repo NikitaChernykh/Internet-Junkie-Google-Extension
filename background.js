9a5aed517c55a5667e70e8f3a34b979fa9e6bbf6
Nikita Chernykh <chernykhmain@gmail.com>
2017-02-13 22:27:36 +0000

    updated analytics scripts

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
+//Google Analytics Start
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-91876786-1']);
+_gaq.push(['_trackPageview']);
+console.log("popup.js loaded");
+
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = 'https://ssl.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+//Google Analytics End
+
+
 //Main Website List
 var websiteList = [];
 
--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-91876786-1']);
 _gaq.push(['_trackPageview']);
-
+console.log("popup.js loaded");
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = 'https://ssl.google-analytics.com/ga.js';
