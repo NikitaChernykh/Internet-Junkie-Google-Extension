34fb26151eeb4ab80b5a1e9c369e3c99461ba0d8
Nikita Chernykh <chernykhmain@gmail.com>
2017-03-28 00:42:42 -0400

    added sort order by visits

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,21 @@
+//Get acsses to the background.js
+    var background = chrome.extension.getBackgroundPage();
+    var content = document.getElementById("websiteList");
+
+    //Sort websites in descending order by visits
+    background.websiteList.sort(function(a, b){
+        return b.websiteVisits - a.websiteVisits;
+    });
+
 (function(){
     var app = angular.module("internetJunkie",[]);
     var MainController = function($scope){
         $scope.remove = function(name){
             console.log( name + "removed");
-        }
-    }
+        };
+        
+        $scope.websites = background.websiteList;
+        $scope.sortOrder = "-websiteVisits";
+    };
     app.controller("MainController", ["$scope",MainController]);
 }());
