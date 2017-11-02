5318d791e670016eae7fb8c720463a04a62daf74
NikitaChernykh <chernykhmain@gmail.com>
2017-11-01 21:47:02 -0400

    scss bundle

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sass = require('gulp-sass');
 
 
 gulp.task('scss', function () {
-  return gulp.src('./app/assets/styles/styles.scss')
+  return gulp.src('./app/assets/scss/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./app/assets/styles/'));
 });
