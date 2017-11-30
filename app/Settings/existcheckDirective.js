544e9cda044cb20725a614e3481f902b7989b7c8
NikitaChernykh <chernykhmain@gmail.com>
2017-11-30 14:38:39 -0500

    added blacklist exist check validation directive

--- app/Settings/existcheckDirective.js
+++ app/Settings/existcheckDirective.js
@@ -1,23 +1,30 @@
-module.exports = function($q) {
+module.exports = function() {
 
   var searchBlacklist = function(name,list){
     for (var i = 0; i < list.length; i++) {
-        if (list[i].websiteName === name) {
-            return true;
+        if (list[i] === name) {
+            return false;
         }
     }
-    return null;
+    return true;
   };
   return{
     restrict: 'A',
     require: 'ngModel',
     link: function (scope, element, attr, ngModel) {
-      //  ngModel.$asyncValidators.invalidEntry = function(modelValue, viewValue) {
-      //      var deferred = $q.defer();
-      //      console.log(searchBlacklist(viewValue,scope.blackList));
-      //      return deferred.promise;// validation logic here
-      //  }
+      console.log("running");
+      ngModel.$parsers.unshift(function(value) {
+        console.log(value);
+        if(value){
+          // test and set the validity
 
+          console.log(scope.blackList);
+          var valid = searchBlacklist(value,scope.blackList);
+          console.log(valid);
+          ngModel.$setValidity('invalidEntry', valid);
+        }
+        return valid ? value : undefined;
+      });
     }
-  }
+  };
 }
