628d7388de9a7ec52e3cebdb8ea6768e90bd5bb9
NikitaChernykh <chernykhmain@gmail.com>
2018-03-07 14:37:13 -0500

    get the app version from manifest

--- app/Shared/footer-component.directive.js
+++ app/Shared/footer-component.directive.js
@@ -1,6 +1,9 @@
 module.exports = function() {
+  function getVersion() {
+    return chrome.runtime.getManifest().version;
+  }
   return{
-      templateUrl: "/Shared/footer.html",
+      template: '<p class="footer--copyright">&copy; Copyright 2017 All Rights Reserved.</p><p class="footer--version">'+getVersion()+'</p>',
       restrict: "E"
   };
 };
