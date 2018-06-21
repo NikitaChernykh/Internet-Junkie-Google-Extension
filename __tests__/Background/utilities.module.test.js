6fa72b663c184e0eecd6ed09ccba1de4ca799cfb
NikitaChernykh <chernykhmain@gmail.com>
2018-06-21 16:51:22 -0400

    test case refactor

--- __tests__/Background/utilities.module.test.js
+++ __tests__/Background/utilities.module.test.js
@@ -1,15 +1,17 @@
 jest.unmock("../../app/Background/utilities.module.js");
 jest.unmock('moment');
 
-
 const UtilitiesModule = require("../../app/Background/utilities.module.js");
 const moment = require('moment');
 
+describe("Utilities Module", () =>{
 
-describe("utilities.module script", () =>{
-  it ("time stamp should return exact time", () => {
-     let timeStampest = moment().format("YYYY-MM-DD HH:mm");
-     let time = UtilitiesModule.timeStamp();
-     expect(timeStampest).toEqual(time);
+  describe("time stamp functionality", () =>{
+    it ("should return exact time", () => {
+        let timeStampTest = moment().format("YYYY-MM-DD HH:mm");
+        let time = UtilitiesModule.timeStamp();
+        expect(timeStampTest).toEqual(time);
+    });
   });
+
 });
