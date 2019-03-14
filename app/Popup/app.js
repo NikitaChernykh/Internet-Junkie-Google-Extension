c9bcd5209f305bca502a20f19c83efb43b2ab335
NikitaChernykh <chernykhmain@gmial.com>
2019-03-14 16:52:01 -0400

    added esling set up and converted app.js according to the rules

--- app/Popup/app.js
+++ app/Popup/app.js
@@ -1,47 +1,78 @@
-var angular = require('angular');
-(function () {
-    'use strict';
-    var app = angular.module("internetJunkie", [require('angular-messages')]);
+const angular = require('angular');
+const angularMessages = require('angular-messages');
+const authService = require('../../app/Login/auth.service');
+const dataService = require('../../app/Shared/data.service');
 
-    //app config for overwriting whitelist ex: for img path
-    app.config(['$compileProvider',function ($compileProvider) {
-          //  Default imgSrcSanitizationWhitelist: /^\s*((https?|ftp|file|blob):|data:image\/)
-          $compileProvider.imgSrcSanitizationWhitelist(/^\s*((https?|ftp|file|blob|chrome-extension):|data:image\/)/);
-        }
-    ]);
+const AppController = require('../../app/Popup/app.controller');
+const HomeController = require('../../app/Home/home.controller');
+const OptionsController = require('../../app/Options/options.controller');
+const SettingsController = require('../../app/Settings/settings.controller');
+const CredentialsController = require('../../app/Login/credentials.controller');
 
-    //services
-    app.factory('authService', require('../../app/Login/auth.service'));
-    app.factory('dataService', require('../../app/Shared/data.service'));
+const weekDaysComponent = require('../../app/Weekdays/week-days-component.directive');
+const footerComponent = require('../../app/Shared/footer-component.directive');
+const langDirective = require('../../app/Shared/lang.directive');
+const loadDaysDirective = require('../../app/Weekdays/load-days.directive');
+const loginViewDirective = require('../../app/Login/login-view.directive');
+const homeViewDirective = require('../../app/Home/home-view.directive');
+const removeblkDirective = require('../../app/Settings/removeblk.directive');
+const settingsViewDirective = require('../../app/Settings/settings-view.directive');
+const noDataMonsterDirective = require('../../app/Home/no-data-monster.directive');
+const noActivityMonsterDirective = require('../../app/Home/no-activity-monster.directive');
+const authDirective = require('../../app/Login/auth.directive');
+const existcheckDirective = require('../../app/Settings/existcheck.directive');
+const removeDirective = require('../../app/Home/remove.directive');
 
-    //controllers
-    app.controller('AppController', require('../../app/Popup/app.controller'));
-    app.controller('HomeController', require('../../app/Home/home.controller'));
-    app.controller('CredentialsController', require('../../app/Login/credentials.controller'));
-    app.controller('OptionsController', require('../../app/Options/options.controller'));
-    app.controller('SettingsController', require('../../app/Settings/settings.controller'));
+const APP_TRANSLATIONS = require('../../app/Shared/constants/translations.constant');
+const APP_VIEWS = require('../../app/Shared/constants/app-views.constant');
+const AUTH_EVENTS = require('../../app/Shared/constants/auth-events.constant');
 
-    //directives
-    app.directive('lang', require('../../app/Shared/lang.directive'));
-    app.directive('loginView', require('../../app/Login/login-view.directive'));
-    app.directive('homeView', require('../../app/Home/home-view.directive'));
-    app.directive('noDataMonster', require('../../app/Home/no-data-monster.directive'));
-    app.directive('noActivityMonster', require('../../app/Home/no-activity-monster.directive'));
-    app.directive('settingsView', require('../../app/Settings/settings-view.directive'));
-    app.directive('auth', require('../../app/Login/auth.directive'));
-    app.directive('remove', require('../../app/Home/remove.directive'));
+(function() {
+  const app = angular.module('internetJunkie', [angularMessages]);
 
-    app.directive('existcheck', require('../../app/Settings/existcheck.directive'));
-    app.directive('removeblk', require('../../app/Settings/removeblk.directive'));
-    app.directive('footerComponent', require('../../app/Shared/footer-component.directive'));
+  // app config for overwriting whitelist ex: for img path
+  app.config([
+    '$compileProvider',
+    function($compileProvider) {
+      //  Default imgSrcSanitizationWhitelist: /^\s*((https?|ftp|file|blob):|data:image\/)
+      $compileProvider.imgSrcSanitizationWhitelist(
+        /^\s*((https?|ftp|file|blob|chrome-extension):|data:image\/)/
+      );
+    },
+  ]);
 
-    app.directive('loadDays', require('../../app/Weekdays/load-days.directive'));
-    app.directive('weekDaysComponent', require('../../app/Weekdays/week-days-component.directive'));
+  // services
+  app.factory('authService', authService);
+  app.factory('dataService', dataService);
 
-    //constants
-    app.constant('AUTH_EVENTS', require('../../app/Shared/constants/auth-events.constant'));
-    app.constant('APP_VIEWS', require('../../app/Shared/constants/app-views.constant'));
-    app.constant('APP_TRANSLATIONS', require('../../app/Shared/constants/translations.constant'));
+  // controllers
+  app.controller('AppController', AppController);
+  app.controller('HomeController', HomeController);
+  app.controller('CredentialsController', CredentialsController);
+  app.controller('OptionsController', OptionsController);
+  app.controller('SettingsController', SettingsController);
 
-    module.exports = app;
-}());
+  // directives
+  app.directive('lang', langDirective);
+  app.directive('loginView', loginViewDirective);
+  app.directive('homeView', homeViewDirective);
+  app.directive('noDataMonster', noDataMonsterDirective);
+  app.directive('noActivityMonster', noActivityMonsterDirective);
+  app.directive('settingsView', settingsViewDirective);
+  app.directive('auth', authDirective);
+  app.directive('remove', removeDirective);
+
+  app.directive('existcheck', existcheckDirective);
+  app.directive('removeblk', removeblkDirective);
+  app.directive('footerComponent', footerComponent);
+
+  app.directive('loadDays', loadDaysDirective);
+  app.directive('weekDaysComponent', weekDaysComponent);
+
+  // constants
+  app.constant('AUTH_EVENTS', AUTH_EVENTS);
+  app.constant('APP_VIEWS', APP_VIEWS);
+  app.constant('APP_TRANSLATIONS', APP_TRANSLATIONS);
+
+  module.exports = app;
+})();
