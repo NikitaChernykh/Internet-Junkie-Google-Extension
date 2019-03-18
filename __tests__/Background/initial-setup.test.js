f2ff69a260015d54aa461f32c449a2b4a353258d
NikitaChernykh <chernykhmain@gmial.com>
2019-03-18 13:54:32 -0400

    added react app

--- __tests__/Background/initial-setup.test.js
+++
@@ -1,8 +0,0 @@
-const appLoadService = require("../../app/Background/appLoadService.js");
-beforeAll(() => {
-  console.log("beforeAll ran");
-});
-
-afterAll(() => {
-  console.log("beforeAll ran");
-});
--- app/Login/auth.directive.js
+++ app/Login/auth.directive.js
@@ -1,42 +1,28 @@
-var APP_VIEWS = require('../../app/Shared/constants/app-views.constant');
+const APP_VIEWS = require('../../app/Shared/constants/app-views.constant');
 
-module.exports = function(authService,APP_VIEWS) {
-  return{
-      link: function (scope,element,attrs,controller){
-          //on state change
-          firebase.auth().onAuthStateChanged(function(user) {
-            if(user){
-                scope.view = APP_VIEWS.homeView;
-                writeUserData(user.uid,user.displayName,user.email,user.photoURL);
-            }else{
-                scope.view = APP_VIEWS.loginView;
-            }
-            authService.view = scope.view;
-            scope.$apply();
-            //chrome.runtime.sendMessage({action: "popup"});
-          });
+module.exports = function(authService, APP_VIEWS) {
+  return {
+    link(scope, element, attrs, controller) {
+      // on state change
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          scope.view = APP_VIEWS.homeView;
+          writeUserData(user.uid, user.displayName, user.email, user.photoURL);
+        } else {
+          scope.view = APP_VIEWS.loginView;
+        }
+        authService.view = scope.view;
+        scope.$apply();
+        // chrome.runtime.sendMessage({action: "popup"});
+      });
 
-          function writeUserData(userId, name, email, imageUrl) {
-
-            database.ref('users/' + userId).set({
-              username: name,
-              email: email,
-              profile_picture : imageUrl
-            });
-          }
-          // //first load
-          // var user = firebase.auth().currentUser;
-          // console.log("first load of auth directive happen");
-          // if(user){
-          //     scope.authenticated = true;
-          //     authService.authenticated = scope.authenticated;
-          //     chrome.runtime.sendMessage({action: "popup"});
-          //     //writeUserData(user.uid,user.displayName,user.email,user.photoURL);
-          // }else{
-          //     scope.authenticated = false;
-          //     authService.authenticated = scope.authenticated;
-          //     chrome.runtime.sendMessage({action: "popup"});
-          // }
+      function writeUserData(userId, name, email, imageUrl) {
+        database.ref(`users/${  userId}`).set({
+          username: name,
+          email,
+          profile_picture: imageUrl,
+        });
       }
+    },
   };
 };
