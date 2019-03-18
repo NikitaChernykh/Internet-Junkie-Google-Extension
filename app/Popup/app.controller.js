422d7a08d6a1005b88421021149498e2712f0cbc
NikitaChernykh <chernykhmain@gmail.com>
2019-03-17 21:42:45 -0400

    app controller changed

--- app/Popup/app.controller.js
+++ app/Popup/app.controller.js
@@ -1,16 +1,13 @@
-var APP_VIEWS = require('../../app/Shared/constants/app-views.constant');
-
-module.exports = function($scope , authService, APP_VIEWS) {
-  'use strict';
-   $scope.view = authService.view;
-   $scope.$on('view', function (event, data) {
-     $scope.view = data;
-   });
-   chrome.runtime.sendMessage({
-       action: "popup"
-   });
-   //open setting page
-  $scope.settings = function(){
+module.exports = function($scope, authService, APP_VIEWS) {
+  $scope.view = authService.view;
+  $scope.$on('view', function(event, data) {
+    $scope.view = data;
+  });
+  chrome.runtime.sendMessage({
+    action: 'popup',
+  });
+  // open setting page
+  $scope.settings = function() {
     _gaq.push(['_trackEvent', 'settingsOpen']);
     authService.view = APP_VIEWS.settingsView;
     $scope.$emit('view', authService.view);
